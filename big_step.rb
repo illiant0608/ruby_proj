29f05a668c849dc06046c17c182074ef34cea617
tengziyue <tengziyue@bytedance.com>
2021-06-09 20:11:49 +0800

    Initial commit

---
+++ big_step.rb
@@ -0,0 +1,25 @@
+class Number
+  def evaluate(environment)
+    self
+  end
+end
+
+class Boolean
+  def evaluate(environment)
+    self
+  end
+end
+
+class Variable
+  def evaluate(environment)
+    environment[name]
+  end
+end
+
+class Add
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+class
